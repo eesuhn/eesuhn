a4fc3eb301319b715cae2568560af738e90210da
eesuhn <eason.yihong@gmail.com>
2024-10-31 00:47:23 +0800

    Fixed image path

--- next-profile/src/components/Background.tsx
+++ next-profile/src/components/Background.tsx
@@ -7,10 +7,14 @@ interface BackgroundProps {
 }
 
 const Background: React.FC<BackgroundProps> = ({ children }) => {
+  const basePath = process.env.NODE_ENV === "production"
+  ? "https://eesuhn.github.io/eesuhn"
+  : "";
+
   return (
     <div
       className="flex min-h-screen items-center justify-center bg-cover bg-center bg-no-repeat p-6"
-      style={{ backgroundImage: `url(/background.svg)` }}
+      style={{ backgroundImage: `url(${basePath}/background.svg)` }}
     >
       {children}
       <ul className="circles">
