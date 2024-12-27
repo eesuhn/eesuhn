4e0ebdd007eea999cf689232486a3e35df45ae2a
eesuhn <eason.yihong@gmail.com>
2024-12-27 20:38:23 +0800

    Updated description

--- next-profile/next.config.ts
+++ next-profile/next.config.ts
@@ -13,18 +13,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        net: false,
-        tls: false,
-      };
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
