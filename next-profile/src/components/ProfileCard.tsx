c0190fa06d7e1aa07762c5904d96a2af1ed2769d
eesuhn <eason.yihong@gmail.com>
2025-01-06 02:31:13 +0800

    Updated resume

--- next-profile/src/components/ProfileCard.tsx
+++ next-profile/src/components/ProfileCard.tsx
@@ -62,7 +62,7 @@ const ProfileCard = () => {
           <div className="mt-16 px-6 py-4 text-center">
             <h2 className="mb-0 text-2xl font-bold text-white">Eason Lim</h2>
             <span className="text-sm text-gray-400">
-              {/* Product @{' '}
+              Product @{' '}
               <a
                 href="https://www.coingecko.com/"
                 target="_blank"
@@ -76,8 +76,7 @@ const ProfileCard = () => {
                   height={24}
                   className="align-cg-logo inline-block rounded-sm"
                 />
-              </a> */}
-              Building in Web3
+              </a>
               {' • '}
               Cadet @{' '}
               <a
